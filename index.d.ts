2b64503d371d07661e67c154cefaff877aecee9c
Chris Talman <cewe.mail@gmail.com>
2019-05-24 13:50:28 +0100

    Introduced additional types.

--- index.d.ts
+++ index.d.ts
@@ -13,6 +13,37 @@ declare module 'paypal-rest-sdk'
 		export class PayPalHttpClient
 		{
 			constructor(environment: LiveEnvironment | SandboxEnvironment);
+			execute(request: PayPalHttpClientRequest);
+		}
+		export interface PayPalHttpClientRequest
+		{
+			path: string;
+			verb: string;
+			body: string;
+			headers:
+			{
+				[name: string]: string;
+			};
+		}
+	}
+	export namespace v1
+	{
+		export namespace webhooks
+		{
+			export class WebhookVerifySignatureRequest
+			{
+				constructor(parameters: WebhookVerifySignatureParameters);
+			}
+			export interface WebhookVerifySignatureParameters
+			{
+				transmission_id: string;
+				transmission_time: string;
+				cert_url: string;
+				auth_algo: string;
+				transmission_sig: string;
+				webhook_id: string;
+				webhook_event: object;
+			}
 		}
 	}
 }
